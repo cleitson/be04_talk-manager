add81d9719ac8bf53ed02141439b388a803686cc
cleitson <cleitson.ftw@gmail.com>
2023-11-09 16:33:54 -0300

    requisito 1

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const fs = require('node:fs').promises;
+const path = require('path');
 
 const router = express.Router();
 
-router.get('/', (_request, response) => {
-  response.status(200).send();
+router.get('/', async (_request, response) => {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, '../talker.json'));
+    const talkers = JSON.parse(data);
+
+    if (!talkers) return response.status(200).send([]);
+
+    response.status(200).send(talkers);
+  } catch (err) {
+    response.status(500).send({ message: err.message });
+  }
 });
 
 module.exports = router;
